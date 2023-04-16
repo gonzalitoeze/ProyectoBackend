8bd4c830d0386cbd36a49f97ec350c9b67af2984
Gonzalo <gonzabarreiro7@gmail.com>
2023-04-16 09:17:18 -0300

    Proyecto Final Backend

---
+++ src/sockets.js
@@ -0,0 +1,9 @@
+import MessageService from "./services/message.service.js";
+import { logger } from "./utils/index.js";
+
+export default (io) => {
+  io.on("connection", async (socket) => {
+    logger.info(`${socket.id} connected.`);
+    io.emit("messages", await MessageService.getAllMessages());
+  });
+};
